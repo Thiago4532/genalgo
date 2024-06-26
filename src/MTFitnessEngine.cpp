2a150aa1fe51b19eedd3259577b51977ae6bb351
Thiago Mota Martins <thiagomota510@gmail.com>
2024-06-26 09:53:40 -0300

    Use OMP_NUM_THREADS instead of hardcode

--- src/MTFitnessEngine.cpp
+++ src/MTFitnessEngine.cpp
@@ -89,10 +89,9 @@ void MTFitnessEngine::evaluate(std::vector<Individual>& individuals){
     
     i32 width = globalCfg.targetImage.getWidth();
     i32 height = globalCfg.targetImage.getHeight();
-    
- 
-    constexpr i32 NUM_THREADS = 16;
-    #pragma omp parallel for num_threads(NUM_THREADS)
+   
+    // Number of threads is controlled by OMP_NUM_THREADS
+    #pragma omp parallel for
     for (i32 i = 0; i < individuals.size(); i++) {
         eval(individuals[i], dst+(i*width*height), src, width, height);
     }
