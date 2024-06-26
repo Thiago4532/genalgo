b789717c662b11c791025bbf0dbf8b5aad04dc5b
Thiago Mota Martins <thiagomota510@gmail.com>
2024-06-26 09:37:06 -0300

    Fixed seed CLI option not working

--- src/main.cpp
+++ src/main.cpp
@@ -62,14 +62,6 @@ int main() {
             return 1;
         }
     } else {
-        // globalCfg.seed = 651999619; // gojo satoru: triangles 10 2500 penalty 0.01
-        // globalCfg.seed = 789671828; // monalisa: triangles 100 2500 penalty 0.001
-        // globalCfg.seed = 142254205;
-        // globalCfg.seed = std::random_device{}();
-        // globalCfg.seed = 2798142837;
-        globalCfg.seed = 23578499;
-    
-        nGen = 1;
         pop.populate();
     }
 
